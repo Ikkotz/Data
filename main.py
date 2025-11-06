232f9dcbaacbe007d11c5c9ad77bb312c082de74
Ikkotz <ikerhc@gmail.com>
2025-11-06 16:10:22 +0100

    Change csv Team Roster vs Lideres historicos ordenados

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 from utils import career, career_df,games_dict,career_json,games,games_json,games_dict
 import pandas as pd
 import json
-
-from nba_api.stats.endpoints import CommonTeamRoster
 from nba_api.stats.endpoints import LeagueLeaders
+from nba_api.stats.endpoints.alltimeleadersgrids import AllTimeLeadersGrids
+import pandas as pd
 
-common_team_roster = CommonTeamRoster(
-    team_id = '1610612739', # input team id
-    league_id_nullable = '00', # nba 00, g_league 20, wnba 10
-    season='2022-23')
-df_common_team_roster = common_team_roster.get_data_frames()[0]
-df_common_team_roster
+data = AllTimeLeadersGrids()
+data_frames = data.get_data_frames()
 
-df_common_team_roster.to_csv("Team Roster.csv", index=False, encoding="utf-8-sig")
-print("✅ Data importada a Team Roster.csv")
+lista = []
+
+for df in data_frames:
+    stat_cols = [c for c in df.columns if c not in ['PLAYER_ID', 'PLAYER_NAME', 'IS_ACTIVE_FLAG'] and not c.endswith('_RANK')]
+    rank_cols = [c for c in df.columns if c.endswith('_RANK')]
+    
+    if not stat_cols or not rank_cols:
+        continue  
+    
+    stat = stat_cols[0]
+    rank_col = rank_cols[0]
+    
+    ordenado = df[['PLAYER_ID', 'PLAYER_NAME', stat, rank_col, 'IS_ACTIVE_FLAG']].copy()
+    ordenado.columns = ['PLAYER_ID', 'PLAYER_NAME', 'VALUE', 'RANK', 'IS_ACTIVE_FLAG']
+    ordenado['CATEGORY'] = stat
+    lista.append(ordenado)
+
+df_final = pd.concat(lista, ignore_index=True)
+
+
+df_final.to_csv("Lideres_historicos_ordenado.csv", index=False, encoding="utf-8-sig")
+print("✅ Archivo 'Lideres_historicos_ordenado.csv' creado correctamente.")
+
 
 league_leaders = LeagueLeaders(
     league_id = '00', # nba 00, g_league 20, wnba 10
-    season = '2022-23', # change year(s) if needed
-    per_mode48 = 'PerGame', # "Totals", "Per48", "PerGame"
+    season = '2022-23', # Se puede cambiar dependiendo del año.
+    per_mode48 = 'Totals', # "Totals", "Per48", "PerGame"
     )
 df_league_leaders = league_leaders.get_data_frames()[0]
 df_league_leaders.to_csv("Lideres de la liga.csv", index=False, encoding="utf-8-sig")
